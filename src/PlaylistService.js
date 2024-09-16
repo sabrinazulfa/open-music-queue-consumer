bf63aa6bfc2a382b2c6bf1c51f2059773613262c
sabrinazulfa <sabrinazulfawa@gmail.com>
2024-09-16 14:51:56 +0700

    update playlist service

--- src/PlaylistService.js
+++ src/PlaylistService.js
@@ -13,28 +13,27 @@ class PlaylistService {
         values: [playlistId],
       };
   
-      const result = await this._pool.query(query);
-      const playlist = result.rows[0];
+    const result = await this._pool.query(query);
+    const playlist = result.rows[0];
   
-      const songsQuery = {
-        text: `
-          SELECT songs.id, songs.title, songs.performer 
-          FROM songs
-          LEFT JOIN playlist_songs ON playlist_songs.song_id = songs.id
-          WHERE playlist_songs.playlist_id = $1
-        `,
-        values: [playlistId],
-      };
+    const songsQuery = {
+      text: `
+        SELECT songs.id, songs.title, songs.performer 
+        FROM songs
+        LEFT JOIN playlist_songs ON playlist_songs.song_id = songs.id
+        WHERE playlist_songs.playlist_id = $1
+      `,
+      values: [playlistId],
+    };
   
-      const songsResult = await this._pool.query(songsQuery);
+    const songsResult = await this._pool.query(songsQuery);
   
-      const songs = songsResult.rows;
-      const response = {
-        ...playlist,
-        songs,
-      };
-  
-      return response;
+    const songs = songsResult.rows;
+    const response = {
+      ...playlist,
+      songs,
+    };
+    return response;
   }
 }
 module.exports = PlaylistService;
