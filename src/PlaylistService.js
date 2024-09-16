d16f05c16162f706f5a2e3174f19a435c74330d9
sabrinazulfa <sabrinazulfawa@gmail.com>
2024-09-16 12:48:18 +0700

    first commit

---
+++ src/PlaylistService.js
@@ -0,0 +1,40 @@
+const { Pool } = require('pg');
+
+class PlaylistService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylists(playlistId) {
+    const query = {
+        text: `
+          SELECT id, name FROM playlists WHERE id = $1
+        `,
+        values: [playlistId],
+      };
+  
+      const result = await this._pool.query(query);
+      const playlist = result.rows[0];
+  
+      const songsQuery = {
+        text: `
+          SELECT songs.id, songs.title, songs.performer 
+          FROM songs
+          LEFT JOIN playlist_songs ON playlist_songs.song_id = songs.id
+          WHERE playlist_songs.playlist_id = $1
+        `,
+        values: [playlistId],
+      };
+  
+      const songsResult = await this._pool.query(songsQuery);
+  
+      const songs = songsResult.rows;
+      const response = {
+        ...playlist,
+        songs,
+      };
+  
+      return response;
+  }
+}
+module.exports = PlaylistService;
